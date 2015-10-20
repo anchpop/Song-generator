c3da5cd662ff41f16f023d82db39e494b9171f6e
anchpop <andre@andreisawesome.com>
2015-10-20 09:14:21 -0400

    made it so you can wrap around the list

--- lyricism.py
+++ lyricism.py
@@ -16,26 +16,34 @@ for i in lines:                                           #generate list of word
     if i.split() != []:
         tokens += i.split()
         
+def list_range(offset, length, l):
+    # this handles both negative offsets and offsets larger than list length
+    start = offset % len(l)
+    end = (start + length) % len(l)
+    if end > start:
+        return l[start:end]
+    return l[start:] + l[:end]
+
 
 def makeStrAlphanumeric(inp):
     return re.sub(r'[a-zA-Z ]+', '', inp)
 
 def makeListAlphanumeric(inp):
-    return map((lambda x: makeStrAlphanumeric(x), inp))
+    return map(makeStrAlphanumeric, inp)
 
 def makemarkov(tokens, level):                            #generates a dictionary in the style of {tuple: Counter} 
     markov = {}                                           #where tuple is the current state and the counter is the words that the state follows
-    for index, word in enumerate(tokens):               
-        if index < level:
-            continue;
+    for index in range(len(tokens)):
+        endIndex = (index+level)
+        word = tokens[endIndex % len(tokens)]
+        toenter = list_range(index, level, tokens)
+        toenter = tuple(toenter)
+        if markov.get(toenter, False):
+            markov[toenter][word] += 1
         else:
-            toenter = tokens[index-level:index]
-            toenter = tuple(toenter)
-            if markov.get(toenter, False):
-                markov[toenter][word] += 1
-            else:
-                markov[toenter] = Counter()
-                markov[toenter][word] += 1
+            markov[toenter] = Counter()
+            markov[toenter][word] += 1
+        
     return markov
 
         
@@ -59,10 +67,18 @@ def getText(length):
     for i in range(0, length):
         currstate += [getRandomItemInCounter(markov, tuple(currstate))]
         currstate = currstate[1:]
-              
         result += currstate[0] + " "
     return result
 
-print(getText(1000))
+print(getText(100))
 
-    
+
+def rhyme(inp, level):
+     entries = corpus.cmudict.entries()
+     syllables = [(word, syl) for word, syl in entries if word == inp]
+     rhymes = []
+     for (word, syllable) in syllables:
+             rhymes += [word for word, pron in entries if pron[-level:] == syllable[-level:]]
+     return set(rhymes)
+
+#print(rhyme('kettle', 1))
